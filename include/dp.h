e9e6c409e77ebdc1ea1ca2cc256abb72c260ace2
MaruKazeMaru <kaze47111@gmail.com>
2023-05-15 19:58:18 +0900

    update dp

--- include/dp.h
+++ include/dp.h
@@ -1,2 +1,8 @@
-// ret[i][0or1] = index of a or b of i th pair
-void matching(int a_size, int b_size, int* a, int* b, int move_cost, int diff_cost, int* ret_size, int*** ret);
+#ifndef _DP_H_
+#define _DP_H_
+
+// ret[i][0/1] = index of a/b of i th pair
+// a must be longer than b
+void matching(size_t a_size, size_t b_size, int* a, int* b, int move_cost, int diff_cost, size_t* ret_size, size_t*** ret, unsigned long* cost);
+
+#endif
--- src/dp.c
+++ src/dp.c
@@ -1,27 +1,70 @@
 #include <stdlib.h>
 #include <math.h>
-#include "dp.h"
 
-void route_to_ret(int a_size, int b_size, char** route, int* ret_size, int*** ret){
-    int n = (a_size + b_size - 1);
-    int** match = (int**)malloc(sizeof(int*) * n);
+#include <dp.h>
 
-    int i = a_size - 1;
-    int j = b_size - 1;
-    int l = 0;
-    int l_max;
+#define COL 'c'
+#define ROW 'r'
+#define SYM 's'
+#define EDG 'e'
+
+char get_route_elem(char** route, unsigned long r, unsigned long c){
+    char mask = 0b00000011;
+    mask <<= 2 * (c % 4);
+    char val = route[r][c / 4] | mask;
+    val >>= 2 * (c % 4);
+    if(val == 0b00000000)
+        return EDG;
+    else if(val == 0b00000001)
+        return COL;
+    else if(val == 0b00000010)
+        return ROW;
+    else if(val == 0b00000011)
+        return SYM;
+    else
+        return EDG;
+}
+
+void set_route_elem(char** route, unsigned long r, unsigned long c, char val){
+    char mask = 0b00000011;
+    mask <<= 2 * (c % 4);
+    route[r][c / 4] &= ~mask;
+
+    if(val == EDG)
+        mask = 0b00000000;
+    else if(val == COL)
+        mask = 0b00000001;
+    else if(val == ROW)
+        mask = 0b00000010;
+    else if(val == SYM)
+        mask = 0b00000011;
+    mask <<= 2 * (c % 4);
+    route[r][c / 4] |= mask;
+
+    return;
+}
+
+void route_to_ret(size_t a_size, size_t b_size, char** route, size_t* ret_size, unsigned long*** ret){
+    size_t n = (a_size + b_size - 1);
+    unsigned long** match = (unsigned long**)malloc(sizeof(unsigned long*) * n);
+
+    unsigned long i = a_size - 1;
+    unsigned long j = b_size - 1;
+    unsigned long l = 0;
+    unsigned long l_max;
     while(1){
-        match[l] = (int*)malloc(sizeof(int) * 2);
+        match[l] = (unsigned long*)malloc(sizeof(unsigned long) * 2);
         match[l][0] = i;
         match[l][1] = j;
-        if(route[i][j] == 's'){
+        char d = get_route_elem(route, i, j);
+        if(d == SYM){
             --i;
             --j;
         }
-        else if(route[i][j] == 'r'){
+        else if(d == ROW){
             --i;
         }
-        else if(route[i][j] == 'c'){
+        else if(d == COL){
             --j;
         }
         else{
@@ -32,9 +75,9 @@ void route_to_ret(int a_size, int b_size, char** route, int* ret_size, int*** re
     }
 
     *ret_size = l_max + 1;
-    *ret = (int**)malloc(sizeof(int*) * *ret_size);
+    *ret = (unsigned long**)malloc(sizeof(unsigned long*) * *ret_size);
     for(l = 0; l <= l_max; ++l){
-        *ret[l] = (int*)malloc(sizeof(int) * 2);
+        *ret[l] = (unsigned long*)malloc(sizeof(unsigned long) * 2);
         *ret[l][0] = match[l_max - l][0];
         *ret[l][1] = match[l_max - l][1];
     }
@@ -46,56 +89,65 @@ void route_to_ret(int a_size, int b_size, char** route, int* ret_size, int*** re
     return;
 }
 
-void matching(int a_size, int b_size, int* a, int* b, int move_cost, int diff_cost, int* ret_size, int*** ret){
-    if(a_size <= 0 || b_size <= 0){
+
+
+void matching(size_t a_size, size_t b_size, int* a, int* b, int move_cost, int diff_cost, size_t* ret_size, unsigned long*** ret, unsigned long* cost){
+    if(a_size <= 0 || b_size <= 0 || a_size < b_size){
         *ret_size = 0;
         return;
     }
 
-    int** dp = (int**)malloc(sizeof(int*) * a_size);
-    char** route = (char**)malloc(sizeof(char*) * a_size);
+    size_t route_row_size = a_size / 4 + 1;
+    size_t route_col_size = b_size / 4 + 1;
+
+    unsigned long** dp = (unsigned long**)malloc(sizeof(unsigned long*) * 2);
+    dp[0] = (unsigned long*)malloc(sizeof(unsigned long) * b_size);
+    dp[1] = (unsigned long*)malloc(sizeof(unsigned long) * b_size);
+    char** route = (char**)malloc(sizeof(char*) * route_row_size);
     for(int i = 0; i < a_size; ++i){
-        dp[i] = (int*)malloc(sizeof(int) * b_size);
-        route[i] = (char*)malloc(sizeof(char) * b_size);
+        route[i] = (char*)calloc(route_col_size, sizeof(char));
     }
 
     // initial value
     dp[0][0] = diff_cost * abs(a[0] - b[0]);
-    route[0][0] = 'e';
-    for(int i = 1; i < a_size; ++i){
-        dp[i][0] = diff_cost * abs(a[i] - b[0]) + move_cost;
-        route[i][0] = 'r';
-    }
-    for(int j = 1; j < a_size; ++j){
-        dp[0][j] = diff_cost * abs(a[0] - b[j]) + move_cost;
-        route[0][j] = 'c';
+    set_route_elem(route, 0, 0, EDG);
+    for(int j = 1; j < b_size; ++j){
+        dp[0][j] = dp[0][j] + diff_cost * abs(a[0] - b[j]) + move_cost;
+        set_route_elem(route, 0, j, COL);
     }
 
     // calc
-    for(int i = 1; i < a_size; ++i){
-        for(int j = 1; j < b_size; ++j){
-            dp[i][j] = diff_cost * abs(a[i] - b[j]);
+    for(unsigned long i = 1; i < a_size; ++i){
+        dp[1][0] = dp[0][0] + move_cost + diff_cost * abs(a[i] - b[0]);
+        set_route_elem(route, i, 0, ROW);
 
-            int r = move_cost + dp[i - 1][j];
-            int c = move_cost + dp[i][j - 1];
-            int s = dp[i - 1][j - 1];
+        for(unsigned long j = 1; j < b_size; ++j){
+            dp[1][j] = diff_cost * abs(a[i] - b[j]);
+
+            int r = move_cost + dp[0][j];
+            int c = move_cost + dp[1][j - 1];
+            int s = dp[0][j - 1];
             if(s <= r && s <= c){
-                dp[i][j] += s;
-                route[i][j] = 's';
+                dp[1][j] += s;
+                set_route_elem(route, i, j, SYM);
             }
             else if(r <= c){
-                dp[i][j] += r;
-                route[i][j] = 'r';
+                dp[1][j] += r;
+                set_route_elem(route, i, j, ROW);
             }
             else{
-                dp[i][j] += c;
-                route[i][j] = 'c';
+                dp[1][j] += c;
+                set_route_elem(route, i, j, COL);
             }
+        }
+
+        for(unsigned long j = 0; j < b_size; ++j){
+            dp[0][j] = dp[1][j];
         }
     }
 
-    for(int i = 0; i < a_size; ++i)
-        free(dp[i]);
+    free(dp[0]);
+    free(dp[1]);
     free(dp);
 
     route_to_ret(a_size, b_size, route, ret_size, ret);
