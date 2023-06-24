a9d958afae7e9c2215a11a4dec510d198e8311e3
MaruKazeMaru <kaze47111@gmail.com>
2023-06-24 15:28:53 +0900

    remove unnecessary files

--- include/dp.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DP_H_
-#define _DP_H_
-
-// route[i][0/1] = index of a/b of i th pair
-void matching(int a_size, int b_size, double** a, double** b, int frame_size, int* route_size, int*** route, double* dist);
-
-#endif
--- include/wavio.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WAVIO_H_
-#define _WAvIO_H_
-
-#include "audio.h"
-
-Audio read_wav(char* file_name, char* has_err, char** err_msg);
-
-#endif
